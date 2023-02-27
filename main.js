cfc63f4d3a089b179c601591de7541faca423426
Matthew Cox <mattdrummerguy@gmail.com>
2023-02-27 10:35:34 -0600

    animations! Cool!

--- main.js
+++ main.js
@@ -66,6 +66,7 @@ function displayPage() {
     //Poll Body - Begin
     pollBody.innerHTML = html` <form>
       <h3 id="pollQuestion">What time of day do you prefer?</h3>
+      <!-- Poll Item 1 - Begin -->
       <div class="pollitem">
         <input type="radio" id="morning" name="poll" value="morning" hidden />
         <label for="morning"
@@ -73,57 +74,52 @@ function displayPage() {
         /></label>
       </div>
       <br />
-
-      <!-- Recommended Time for Office Hours -->
-
-      <!-- Poll Item 1 - Begin -->
-      <div class="pollitem">
-        <input
-          type="radio"
-          id="afternoon"
-          name="poll"
-          value="afternoon"
-          hidden
-        />
-        <label for="afternoon"
-          ><img
-            id="afternoonImg"
-            src="./images/afternoon.png"
-            alt="afternoon"
+      <div class="pollItemContainer">
+        <!-- Poll Item  1 - End -->
+        <!-- Poll Item 2 - Begin -->
+        <div class="pollitem">
+          <input
+            type="radio"
+            id="afternoon"
+            name="poll"
+            value="afternoon"
+            hidden
           />
-        </label>
-      </div>
-      <br />
-      <div class="pollitem">
-        <input type="radio" id="evening" name="poll" value="evening" hidden />
-        <label for="evening"
-          ><img id="eveningImg" src="./images/evening.png" alt="evening" />
-        </label>
-      </div>
-      <!-- Poll Item 1 - End -->
-      <!-- POll Item 2 - Begin -->
-      <div class="pollitem">
-        <!-- type of input should be a text area -->
-
-        <label for="other"
-          >Within each day of the week, select one or more times you would like
-          to see office hours. <br />
-        </label>
-
-        <!-- Office Proposed Times - End -->
+          <label for="afternoon"
+            ><img
+              id="afternoonImg"
+              src="./images/afternoon.png"
+              alt="afternoon"
+            />
+          </label>
+        </div>
+        <br />
+        <div class="pollitem">
+          <input type="radio" id="evening" name="poll" value="evening" hidden />
+          <label for="evening"
+            ><img id="eveningImg" src="./images/evening.png" alt="evening" />
+          </label>
+        </div>
+        <!-- Poll Item 2 - End -->
+        <!-- Poll Item 3 - Begin -->
+        <div class="pollitem">
+          <!-- type of input should be a text area -->
+        </div>
+        <h3>
+          Within each day of the week, select one or more times you would like
+          to see office hours.
+        </h3>
         <div>
-          <!-- Monday - Begin -->
-          <div class="date" id="Monday">
-            <h4>Monday:</h4>
-            <!-- display multiple checkbox containing times throughout the day -->
-            <!-- include EST CST MST PST -->
+          <!-- Office Proposed Times - End -->
 
-            <div id="pollBodyTimes"></div>
-          </div>
-          <!-- Monday - End -->
+          <!-- display multiple checkbox containing times throughout the day -->
+          <!-- include EST CST MST PST -->
+
+          <div id="pollBodyTimes"></div>
         </div>
         <!-- Office Proposed Times - End -->
       </div>
+      <!-- Poll Item 3 - End -->
 
       <br />
       <input id="submit" type="button" value="Submit" />
@@ -149,52 +145,96 @@ function displayPage() {
     let pollBodyTimes = document.getElementById("pollBodyTimes"); //this is the div that will hold the times
     // console.log("pollBodyTimes:", pollBodyTimes);
 
+    //NOTE Morning Event Listener - Begin
     let morning = document.getElementById("morning");
-    // Morning Event Listener - Begin
     morning.addEventListener("click", function () {
       // console.log("morning clicked");
       // console.log("pollBodyTimes:", pollBodyTimes);
 
-      //clear pollBodyTimes first
       pollBodyTimes.innerHTML = ""; //this clears the div adding new data to it each time
-
+      pollBodyTimes.style.transition = "opacity 0.5s ease-in-out";
       pollBodyTimes.style.opacity = 0;
-      pollBodyTimes.innerHTML = html`<!-- Morning Times - Begin -->
-        <div class="officeDateTimes" id="morning-times">
-          <h5>Morning</h5>
-          <p>
-            <label>10pm (EST) 9am (CST) 8am (MST) 7am (PST)</label>
-            <input
-              type="checkbox"
-              id="monday1"
-              name="monday"
-              value="10pm (EST) 9am (CST) 8am (MST) 7am (PST)"
-            />
-          </p>
-          <p>
-            <label>11pm (EST) 10am (CST) 9am (MST) 8am (PST)</label>
-            <input
-              type="checkbox"
-              id="monday2"
-              name="monday"
-              value="11pm (EST) 10am (CST) 9am (MST) 8am (PST)"
-            />
-          </p>
-          <p>
-            <label>12pm (EST) 11am (CST) 10am (MST) 9am (PST)</label>
-            <input
-              type="checkbox"
-              id="monday3"
-              name="monday"
-              value="12pm (EST) 11am (CST) 10am (MST) 9am (PST)"
-            />
-          </p>
-        </div>
-        <!-- Morning Times - End -->`;
+
       setTimeout(function () {
-        pollBodyTimes.style.transition = "opacity 1s ease-in-out";
+        pollBodyTimes.style.border = "none";
         pollBodyTimes.style.opacity = 1;
-      }, 0);
+        pollBodyTimes.innerHTML = html`<!-- Morning Times - Begin -->
+          <!-- Monday - Begin -->
+          <div class="date expand-animation" id="Monday">
+            <h4>Monday:</h4>
+
+            <div class="officeDateTimes" id="morning-times">
+              <p>
+                <label>10pm (EST) 9am (CST) 8am (MST) 7am (PST)</label>
+                <input
+                  type="checkbox"
+                  id="monday1"
+                  name="monday"
+                  value="10pm (EST) 9am (CST) 8am (MST) 7am (PST)"
+                />
+              </p>
+              <p>
+                <label>11pm (EST) 10am (CST) 9am (MST) 8am (PST)</label>
+                <input
+                  type="checkbox"
+                  id="monday2"
+                  name="monday"
+                  value="11pm (EST) 10am (CST) 9am (MST) 8am (PST)"
+                />
+              </p>
+              <p>
+                <label>12pm (EST) 11am (CST) 10am (MST) 9am (PST)</label>
+                <input
+                  type="checkbox"
+                  id="monday3"
+                  name="monday"
+                  value="12pm (EST) 11am (CST) 10am (MST) 9am (PST)"
+                />
+              </p>
+            </div>
+          </div>
+          <!-- Monday - End -->
+          <!-- Tuesday - Begin -->
+          <div class="date" id="Tuesday">
+            <h4>Tuesday:</h4>
+
+            <div class="officeDateTimes" id="morning-times">
+              <p>
+                <label>10pm (EST) 9am (CST) 8am (MST) 7am (PST)</label>
+                <input
+                  type="checkbox"
+                  id="tuesday1"
+                  name="tuesday"
+                  value="10pm (EST) 9am (CST) 8am (MST) 7am (PST)"
+                />
+              </p>
+              <p>
+                <label>11pm (EST) 10am (CST) 9am (MST) 8am (PST)</label>
+                <input
+                  type="checkbox"
+                  id="tuesday2"
+                  name="tuesday"
+                  value="11pm (EST) 10am (CST) 9am (MST) 8am (PST)"
+                />
+              </p>
+              <p>
+                <label>12pm (EST) 11am (CST) 10am (MST) 9am (PST)</label>
+                <input
+                  type="checkbox"
+                  id="tuesday3"
+                  name="tuesday"
+                  value="12pm (EST) 11am (CST) 10am (MST) 9am (PST)"
+                />
+              </p>
+            </div>
+            <!-- Tuesday - End -->
+          </div>
+          <!-- Morning Times - End -->`;
+        pollBodyTimes.style.transition =
+          "opacity 1s ease-in-out, border 0s ease-in-out";
+        pollBodyTimes.style.border = "1px solid white";
+        pollBodyTimes.style.borderRadius = "20px";
+      }, 500);
 
       body.classList.add("yellow");
       //   fade in background to yellow over 3 seconds
@@ -209,56 +249,98 @@ function displayPage() {
       body.style.backgroundColor = "#CBBD9B";
       body.style.color = "black";
 
-      window.scrollTo(-1, document.body.scrollHeight);
+      window.scrollTo(-1, document.body.scrollHeight); //Scrolls to the bottom of the page
     });
     //Morning Event Listener - End
 
-    //Afternoon
+    //NOTE Afternoon Event Listener - Begin
     let afternoon = document.getElementById("afternoon");
     afternoon.addEventListener("click", function () {
       // console.log("afternoon clicked");
       // console.log("pollBodyTimes:", pollBodyTimes);
 
-      //clear pollBodyTimes first
       pollBodyTimes.innerHTML = ""; //this clears the div adding new data to it each time
+      pollBodyTimes.style.transition = "opacity 0.5s ease-in-out";
       pollBodyTimes.style.opacity = 0;
 
-      pollBodyTimes.innerHTML = html` <!-- Afternoon Times - Begin -->
-        <div class="officeDateTimes" id="afternoon-times">
-          <h5>Afternoon</h5>
-          <p>
-            <label>2pm (EST) 1pm (CST) 12pm (MST) 11pm (PST)</label>
-            <input
-              type="checkbox"
-              id="monday1"
-              name="monday"
-              value="2pm (EST) 1pm (CST) 12pm (MST) 11pm (PST)"
-            />
-          </p>
-          <p>
-            <label>3pm (EST) 2pm (CST) 1pm (MST) 12pm (PST)</label>
-            <input
-              type="checkbox"
-              id="monday2"
-              name="monday"
-              value="3pm (EST) 2pm (CST) 1pm (MST) 12pm (PST)"
-            />
-          </p>
-          <p>
-            <label>4pm (EST) 3pm (CST) 2pm (MST) 1pm (PST)</label>
-            <input
-              type="checkbox"
-              id="monday3"
-              name="monday"
-              value="4pm (EST) 3pm (CST) 2pm (MST) 1pm (PST)"
-            />
-          </p>
-        </div>
-        <!-- Afternoon Times - End -->`;
       setTimeout(function () {
-        pollBodyTimes.style.transition = "opacity 1s ease-in-out";
+        pollBodyTimes.style.border = "none";
         pollBodyTimes.style.opacity = 1;
-      }, 0);
+        pollBodyTimes.innerHTML = html` <!-- Afternoon Times - Begin -->
+          <!-- Monday - Begin -->
+          <div class="date" id="Monday">
+            <h4>Monday:</h4>
+            <div class="officeDateTimes expand-animation" id="afternoon-times">
+              <p>
+                <label>2pm (EST) 1pm (CST) 12pm (MST) 11pm (PST)</label>
+                <input
+                  type="checkbox"
+                  id="monday1"
+                  name="monday"
+                  value="2pm (EST) 1pm (CST) 12pm (MST) 11pm (PST)"
+                />
+              </p>
+              <p>
+                <label>3pm (EST) 2pm (CST) 1pm (MST) 12pm (PST)</label>
+                <input
+                  type="checkbox"
+                  id="monday2"
+                  name="monday"
+                  value="3pm (EST) 2pm (CST) 1pm (MST) 12pm (PST)"
+                />
+              </p>
+              <p>
+                <label>4pm (EST) 3pm (CST) 2pm (MST) 1pm (PST)</label>
+                <input
+                  type="checkbox"
+                  id="monday3"
+                  name="monday"
+                  value="4pm (EST) 3pm (CST) 2pm (MST) 1pm (PST)"
+                />
+              </p>
+            </div>
+          </div>
+          <!-- Monday - End -->
+          <!-- Tuesday - Begin -->
+          <div class="date" id="Tuesday">
+            <h4>Tuesday:</h4>
+            <div class="officeDateTimes" id="afternoon-times">
+              <p>
+                <label>2pm (EST) 1pm (CST) 12pm (MST) 11pm (PST)</label>
+                <input
+                  type="checkbox"
+                  id="tuesday1"
+                  name="tuesday"
+                  value="2pm (EST) 1pm (CST) 12pm (MST) 11pm (PST)"
+                />
+              </p>
+              <p>
+                <label>3pm (EST) 2pm (CST) 1pm (MST) 12pm (PST)</label>
+                <input
+                  type="checkbox"
+                  id="tuesday2"
+                  name="tuesday"
+                  value="3pm (EST) 2pm (CST) 1pm (MST) 12pm (PST)"
+                />
+              </p>
+              <p>
+                <label>4pm (EST) 3pm (CST) 2pm (MST) 1pm (PST)</label>
+                <input
+                  type="checkbox"
+                  id="tuesday3"
+                  name="tuesday"
+                  value="4pm (EST) 3pm (CST) 2pm (MST) 1pm (PST)"
+                />
+              </p>
+            </div>
+            <!-- Monday - End -->
+          </div>
+          <!-- Afternoon Times - End -->`;
+        pollBodyTimes.style.transition =
+          "opacity 1s ease-in-out, border 0s ease-in-out";
+        pollBodyTimes.style.border = "1px solid black";
+        pollBodyTimes.style.borderRadius = "20px";
+      }, 500);
 
       body.classList.add("orange");
       //   fade in background to orange over 3 seconds
@@ -272,54 +354,68 @@ function displayPage() {
       body.style.transition = "color background-color 3s ease-in-out";
       body.style.backgroundColor = "#b17869";
       body.style.color = "black";
-    });
 
-    //Evening
+      window.scrollTo(-1, document.body.scrollHeight); //Scrolls to the bottom of the page
+    });
+    //Afternoon Event Listener - End
+
+    //NOTE Evening Event Listener - Begin
     let evening = document.getElementById("evening");
     evening.addEventListener("click", function () {
       // console.log("evening clicked");
       // console.log("pollBodyTimes:", pollBodyTimes);
 
-      //clear pollBodyTimes first
       pollBodyTimes.innerHTML = ""; //this clears the div adding new data to it each time
+      pollBodyTimes.style.transition = "opacity 0.5s ease-in-out";
       pollBodyTimes.style.opacity = 0;
 
-      pollBodyTimes.innerHTML = html` <!-- Evening Times - Begin -->
-        <div class="officeDateTimes" id="evening-times">
-          <h5>Evening</h5>
-          <p>
-            <label>5pm (EST) 4pm (CST) 3pm (MST) 2pm (PST)</label>
-            <input
-              type="checkbox"
-              id="monday1"
-              name="monday"
-              value="5pm (EST) 4pm (CST) 3pm (MST) 2pm (PST)"
-            />
-          </p>
-          <p>
-            <label>6pm (EST) 5pm (CST) 4pm (MST) 3pm (PST)</label>
-            <input
-              type="checkbox"
-              id="monday2"
-              name="monday"
-              value="6pm (EST) 5pm (CST) 4pm (MST) 3pm (PST)"
-            />
-          </p>
-          <p>
-            <label>7pm (EST) 6pm (CST) 5pm (MST) 4pm (PST)</label>
-            <input
-              type="checkbox"
-              id="monday3"
-              name="monday"
-              value="7pm (EST) 6pm (CST) 5pm (MST) 4pm (PST)"
-            />
-          </p>
-        </div>
-        <!-- Evening Times - End -->`;
       setTimeout(function () {
-        pollBodyTimes.style.transition = "opacity 1s ease-in-out";
+        pollBodyTimes.style.border = "none";
         pollBodyTimes.style.opacity = 1;
-      }, 0);
+        pollBodyTimes.innerHTML = html` <!-- Evening Times - Begin -->
+        <!-- Monday - Begin -->
+        <div class="date expand-animation" id="Monday">
+          <h4>Monday:</h4>
+          <div class="officeDateTimes" id="evening-times">
+            <h5>Evening</h5>
+            <p>
+              <label>5pm (EST) 4pm (CST) 3pm (MST) 2pm (PST)</label>
+              <input
+                type="checkbox"
+                id="monday1"
+                name="monday"
+                value="5pm (EST) 4pm (CST) 3pm (MST) 2pm (PST)"
+              />
+            </p>
+            <p>
+              <label>6pm (EST) 5pm (CST) 4pm (MST) 3pm (PST)</label>
+              <input
+                type="checkbox"
+                id="monday2"
+                name="monday"
+                value="6pm (EST) 5pm (CST) 4pm (MST) 3pm (PST)"
+              />
+            </p>
+            <p>
+              <label>7pm (EST) 6pm (CST) 5pm (MST) 4pm (PST)</label>
+              <input
+                type="checkbox"
+                id="monday3"
+                name="monday"
+                value="7pm (EST) 6pm (CST) 5pm (MST) 4pm (PST)"
+              />
+            </p>
+          </div>
+          </div>
+   <!-- Monday - End -->
+        </div>
+        
+        <!-- Evening Times - End -->`;
+        pollBodyTimes.style.transition =
+          "opacity 1s ease-in-out, border 0s ease-in-out";
+        pollBodyTimes.style.border = "1px solid white";
+        pollBodyTimes.style.borderRadius = "20px";
+      }, 500);
 
       body.classList.add("blue");
       //   fade in background to blue over 3 seconds
@@ -333,7 +429,10 @@ function displayPage() {
       body.style.transition = "color background-color 3s ease-in-out";
       body.style.backgroundColor = "#5970a9";
       body.style.color = "white";
+
+      window.scrollTo(-1, document.body.scrollHeight); //Scrolls to the bottom of the page
     });
+    //Evening Event Listener - End
 
     getData();
   }
